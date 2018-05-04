b09ebb889b7abd8f2e152535b052f2a34ea28076
Alexander Iansiti <aiansiti@outlook.com>
2018-05-04 01:40:20 -0400

    add port arg

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import { default as Web3 } from 'web3';
+import express from "express";
+import { default as Web3 } from "web3";
 import { default as contract } from "truffle-contract";
-import sha256 from 'js-sha256';
-import hat from 'hat';
-import geoip from 'geoip-lite';
-import os from 'os';
+import sha256 from "js-sha256";
+import hat from "hat";
+import geoip from "geoip-lite";
+import os from "os";
 
 import auction from "./build/contracts/dataAuction.json";
-import auctionFactory from "./build/contracts/AuctionFactory.json"
+import auctionFactory from "./build/contracts/AuctionFactory.json";
 import sampleMetadata from "./sampleMetadata.json";
-import tempData from './temperature.json';
-import humidityData from './humidity.json';
+import tempData from "./temperature.json";
+import humidityData from "./humidity.json";
 
 const app = express();
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
@@ -24,24 +24,36 @@ const beneficiary = web3.eth.accounts[0];
 let auctionAddress = null;
 
 const createAuction = async () => {
-  let sellerHash = '0x' + sha256(JSON.stringify(tempData).slice(0, 128));
+  let sellerHash = "0x" + sha256(JSON.stringify(tempData).slice(0, 128));
   let collectionPeriod = 600;
   let biddingTime = 600;
 
-  let metadataJSON = sampleMetadata[0];
+  let metadataJSON = sampleMetadata[process.env.PORT];
   metadataJSON.location = getLocation();
   let metadata = JSON.stringify(metadataJSON);
 
-  const unlocked = await web3.personal.unlockAccount(beneficiary, 'welcome123', 10);
+  const unlocked = await web3.personal.unlockAccount(
+    beneficiary,
+    "welcome123",
+    10
+  );
   console.log("API_KEY: " + apiKey);
 
   let factoryInstance = await AuctionFactory.deployed();
-  let auction = await factoryInstance.createAuction(biddingTime, beneficiary, collectionPeriod, sellerHash, metadata, apiKey, { gas: 1500000, from: beneficiary });
+  let auction = await factoryInstance.createAuction(
+    biddingTime,
+    beneficiary,
+    collectionPeriod,
+    sellerHash,
+    metadata,
+    apiKey,
+    { gas: 1500000, from: beneficiary }
+  );
   auctionAddress = auction.logs[0].address;
   return auction;
 };
 
-const getAuctions = async (req,res,next) => {
+const getAuctions = async (req, res, next) => {
   let factoryInstance = await AuctionFactory.deployed();
   let auction = await factoryInstance.getAuction.call(0);
   req.data = auction;
@@ -52,17 +64,17 @@ const getLocation = () => {
   let ifaces = os.networkInterfaces();
   let location = null;
 
-  Object.keys(ifaces).forEach(function (ifname) {
-    ifaces[ifname].forEach(function (iface) {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
+  Object.keys(ifaces).forEach(function(ifname) {
+    ifaces[ifname].forEach(function(iface) {
+      if ("IPv4" !== iface.family || iface.internal !== false) {
         // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
         return;
       }
       let ip = iface.address;
-      location = geoip.lookup(ip) || geoip.lookup('207.97.227.239');
-      location.zip = location.zip.toString().padStart(5,"0");
-      delete location['range'];
-      delete location['metro'];
+      location = geoip.lookup(ip) || geoip.lookup("207.97.227.239");
+      location.zip = location.zip.toString().padStart(5, "0");
+      delete location["range"];
+      delete location["metro"];
     });
   });
 
@@ -79,7 +91,7 @@ const withdrawFunds = async () => {
         await auction.withdrawReward({ from: beneficiary });
         clearInterval(intervalId);
       } catch (error) {
-        console.log('Not ready yet')
+        console.log("Not ready yet");
       }
     }
   }
@@ -94,7 +106,7 @@ const endAuction = async () => {
       try {
         await auction.endAuction({ from: beneficiary });
       } catch (error) {
-        console.log('Could not end auction');
+        console.log("Could not end auction");
       }
     }
   }
@@ -102,35 +114,35 @@ const endAuction = async () => {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "x-access-token");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "x-access-token"
+  );
   next();
 });
 
-app.get('/getAuctions', getAuctions, (req, res) => {
+app.get("/getAuctions", getAuctions, (req, res) => {
   const result = req.data;
   res.send(result);
 });
 
-app.get('/things/pi/properties/temperature', (req, res) => {
-  if (req.get('x-access-token') !== apiKey)
-    return res.sendStatus(401);
-  res.send(tempData)
-})
+app.get("/things/pi/properties/temperature", (req, res) => {
+  if (req.get("x-access-token") !== apiKey) return res.sendStatus(401);
+  res.send(tempData);
+});
 
-app.get('/things/pi/properties/humidity', (req, res) => {
-  if (req.get('x-access-token') !== apiKey)
-    return res.sendStatus(401);
-  res.send(humidityData)
-})
+app.get("/things/pi/properties/humidity", (req, res) => {
+  if (req.get("x-access-token") !== apiKey) return res.sendStatus(401);
+  res.send(humidityData);
+});
 
-
-app.listen(5000, async () => {
-    await createAuction();
-    withdrawFunds();
-    endAuction();
-    console.log('IoT device listening on port 5000!')
-  });
-
+app.listen(5000 + process.env.PORT, async () => {
+  await createAuction();
+  withdrawFunds();
+  endAuction();
+  console.log(`IoT device listening on port ${5000 + process.env.PORT}!`);
+});
 
 /*
 function createExampleContract() {
