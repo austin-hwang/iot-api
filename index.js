3c54f37102889643c430002816b37e13c4f08cdf
Alexander Iansiti <aiansiti@outlook.com>
2018-05-05 01:56:48 -0400

    change ip for different ports

--- index.js
+++ index.js
@@ -81,7 +81,10 @@ const getLocation = () => {
         return;
       }
       let ip = iface.address;
-      location = geoip.lookup(ip) || geoip.lookup("207.97.227.239");
+      location =
+        geoip.lookup(ip) || process.env.PORT < 2
+          ? geoip.lookup("207.97.227.239")
+          : geoip.lookup("65.112.8.132");
       location.zip = location.zip.toString().padStart(5, "0");
       delete location["range"];
       delete location["metro"];
